8920d2d814536ebccbd43ec22810f1f0d19bf42a
kenocvr <kenocvr@gmail.com>
2018-04-21 21:28:42 -0700

    Update layouts

--- app/src/main/java/tech/labs/rucker/llamachat/View/ContactsActivity.java
+++ app/src/main/java/tech/labs/rucker/llamachat/View/ContactsActivity.java
@@ -1,7 +1,6 @@
 package tech.labs.rucker.llamachat.View;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -13,7 +12,6 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -30,37 +28,8 @@ import tech.labs.rucker.llamachat.R;
 
 public class ContactsActivity extends AppCompatActivity {
 
-
-    // Todo(1): Item click listener... myRoomStr.OnClickListener => Intent/Extras => MessageActivity
-
-        /*
-        *   ChatRoom Schema
-        * */
-        /*
-        *User rooms -- Display in client side RecyclerView/list
-        * */
-    //(ROOT)userID:
-        // Rooms:
-            // *****: myRoomStr0
-            // *****: myRoomStr1
-        /*
-        * Rooms with chat messages. Root Level. All clients can add(read) and write to.
-        * */
-    //(ROOT)Rooms:
-        // myRoomStr0:
-            // ******: "Carlos says: Doood! The Chat room works!!!"
-            // ******: "D says: ikr, so cool!"
-
-        // myRoomStr1:
-
-        // herRoomStr4: "No one knows about this room yet!"
-
-        // Edit Text
-        // Firebase Database ⇒ push() ⇒  UserID.rooms
-
     EditText roomEditText;
     Button button;
-    Button messageBtn;
     FirebaseAuth mAuth;
     FirebaseAuth.AuthStateListener mAuthListener;
     private String value;
@@ -80,15 +49,11 @@ public class ContactsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
-        //writeUserToList("Users");
-
         button = findViewById(R.id.signOut);
-        messageBtn = findViewById(R.id.messageBtn);
         roomEditText = findViewById(R.id.roomEditText);
         recyclerView = findViewById(R.id.contactsRecycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         contactsView();
-        //addUserToList();
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -106,24 +71,12 @@ public class ContactsActivity extends AppCompatActivity {
             }
         });
 
-        messageBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(ContactsActivity.this, MessageActivity.class));
-            }
-        });
-
-
     }
     public void contactsView(){
         FirebaseDatabase databaseInstance = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference;
 
-        //String userId = mAuth.getUid();
         String urlPath = "https://llamachat-a4865.firebaseio.com/cFTFT9wNF9aLRpijxpmuncTbETt2/Rooms";
-        //"https://llamachat-a4865.firebaseio.com/" + userId + "/Rooms";
-        // Get reference to User's rooms list in:
-        // userId: rooms: ...
         databaseReference = databaseInstance.getReferenceFromUrl(urlPath);
 
         databaseReference.addValueEventListener(new ValueEventListener() {
@@ -139,7 +92,6 @@ public class ContactsActivity extends AppCompatActivity {
                     Log.d("Message Data::",roomName);
 
                     // Todo: Clicking chat room item. Pass extras: roomName, etc
-                    // Follow pattern: https://android.jlelse.eu/click-listener-for-recyclerview-adapter-2d17a6f6f6c9
                 }
             }
 
@@ -149,96 +101,18 @@ public class ContactsActivity extends AppCompatActivity {
             }
         });
 
-
-    }
-//    public void writeToDatabase(String parent, String child, String value){
-//
-//        DatabaseHelper roomName = new DatabaseHelper();
-//        roomName.setParentNode("parentRoom").setChildNode("childRoom").setChildValue("valueRoom");
-//
-//
-//        //.value = value;
-//       // final FirebaseAuth mAuth = FirebaseAuth.getInstance();
-//        //final FirebaseDatabase database = FirebaseDatabase.getInstance();
-//        //final DatabaseReference db;
-//       // db = database.getReference(parent).child(child);
-//       // final String msgmsg = db.push()
-//        //        .setValue(value)
-//        //        .toString();
-//       // final String msgkey = db.getKey();
-//    }
-
-    public void writeUserToList(final String param){
-        final FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference messageRecipient;
-                messageRecipient = database
-                        .getReference()
-                        .child(param);
-        final String messageRecipientStr = messageRecipient.getKey();
-        final String msgmsg = messageRecipient.push()
-                .setValue(mAuth.getCurrentUser()
-                        .getDisplayName())
-                        .toString();
     }
 
-    public void addUserToList(){
-        // Get and Write current user to list of users
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference mRef;
-        mRef = database
-                .getReference("users")
-                .child("TESTEMAIL");
-        final String clientSideKey = mRef.getKey();
-
-        if (user != null) {
-            // Name, email address, and profile photo Url
-            String name = user.getDisplayName();
-            String email = user.getEmail();
-            Uri photoUrl = user.getPhotoUrl();
-            // Check if user's email is verified
-            boolean emailVerified = user.isEmailVerified();
-            String uid = user.getUid();
-        }
-    }
-//    public void initView(){
-//        listItems = new ArrayList<>();
-//        for(int i = 0; i <=10; i++){
-//            ListItem listItem = new ListItem(
-//                    "heading" +(i+1),
-//                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-//            );
-//            listItems.add(listItem);
-//        }
-//
-//        adapter = new ContactsAdapter(listItems, ContactsActivity.this);
-//        recyclerView.setAdapter(adapter);
-//    }
 
     public void addRoom(View view) {
         String roomStr = roomEditText.getText().toString();
         DatabaseHelper roomName = new DatabaseHelper();
-
-        // Todo: Duplicate entry check.
-        // Check if entry does not exist in database
-        // if (entry == null){ roomName...push(); }
         roomName.setParentNode("parentRm").setChildNode("childRm").setChildValue(roomStr).push();
         roomName.setParentNode("Rooms").setChildNode(roomStr).setChildValue("Welcome!").push();
         DatabaseHelper usersRooms = new DatabaseHelper();
         DatabaseHelper roomMessages = new DatabaseHelper();
-        // Todo: RecyclerView based on these room values.
         // User's rooms
         usersRooms.setParentNode(mAuth.getUid()).setChildNode("Rooms").setChildValue(roomStr).push();
-        // Todo: Check if room exists. Append messages. Avoid deleting messages with blank room creation.
-        // If room != null, initialize room with welcome message.
-        // //JSON tree requires children with messages
-        // Chat room schema //
-        // Rooms
-        // <roomStr>
-        // ****:<message>
-        // ****:<message>
-
         // Creates room with welcome message
         roomMessages.setParentNode("Rooms").setChildNode(roomStr).setChildValue("Welcome!");
     }
--- app/src/main/java/tech/labs/rucker/llamachat/View/MessageActivity.java
+++ app/src/main/java/tech/labs/rucker/llamachat/View/MessageActivity.java
@@ -12,7 +12,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -28,7 +27,8 @@ import tech.labs.rucker.llamachat.R;
 
 public class MessageActivity extends AppCompatActivity {
 
-    // Todo: Receive extras from "room" selection in Contacts recyclerView
+    // Todo: Delete room from list with long-press and/or button.
+    // Todo: Check for empty-text room. Text input should not be empty.
     // Todo: Hide Keyboard on Send
     // Todo: Fix Input field Overlap in UI
     // Todo: Scroll to bottom of List Automatically
@@ -74,29 +74,15 @@ public class MessageActivity extends AppCompatActivity {
         return emptyStr;
     }
 
-
-    public String displayUser() {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        return user.getDisplayName();
-    }
-    public String displayUserId(){
-        FirebaseAuth userId = FirebaseAuth.getInstance();
-        return userId.toString();
-    }
     public void initMessage(){
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         final String name = mAuth.getCurrentUser().getDisplayName();
         final String uId = mAuth.getCurrentUser().getUid();
         final DatabaseReference messageRecipient;
-        //String roomName = getIntent().getStringExtra("ROOM_NAME");
 
-//        messageRecipient = database
-//                .getReference(uId)
-//                .child("messages");
         messageRecipient = database
                 .getReferenceFromUrl("https://llamachat-a4865.firebaseio.com/Rooms/" + checkIntent());
-                //.getReference(roomName);
 
         final String msgKey = messageRecipient.push().getKey();
         final String clientSideKey = messageRecipient.getKey();
@@ -107,7 +93,7 @@ public class MessageActivity extends AppCompatActivity {
                 listItems = new ArrayList<>();
                 for (DataSnapshot msgSnapshot : dataSnapshot.getChildren()){
                     String msg0 = msgSnapshot.getValue().toString();
-                    ListItem listItem = new ListItem(name, msg0);
+                    ListItem listItem = new ListItem(" ", msg0);
                     listItems.add(listItem);
                     adapter = new MessageAdapter(listItems, MessageActivity.this);
                     recyclerView.setAdapter(adapter);
@@ -121,50 +107,7 @@ public class MessageActivity extends AppCompatActivity {
             }
         });
     }
-    public void sendMessage(){
-        final FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        sendBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final DatabaseReference messageRecipient;
-                final String message = messageText.getText().toString();
-                //final String uId = mAuth.getUid();
-                final String name = mAuth.getCurrentUser().getDisplayName();
-                final String uId = mAuth.getCurrentUser().getUid();
 
-                messageRecipient = database
-                        .getReference(uId)
-                        .child("messages");
-                 final String msgKey = messageRecipient.push().getKey();
-                 final String msgmsg = messageRecipient.push().setValue(message).toString();
-                 final String clientSideKey = messageRecipient.getKey();
-
-               messageRecipient.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        listItems = new ArrayList<>();
-                        for (DataSnapshot msgSnapshot : dataSnapshot.getChildren()){
-                            String msg0 = msgSnapshot.getValue().toString();
-                            ListItem listItem = new ListItem(name, msg0);
-                            listItems.add(listItem);
-
-                            adapter = new MessageAdapter(listItems, MessageActivity.this);
-                            recyclerView.setAdapter(adapter);
-                                Log.d("Message Data::",msg0);
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
-
-            }
-        });
-
-    }
 
     public void sendMessage(final String contactId){
         final FirebaseAuth mAuth = FirebaseAuth.getInstance();
@@ -178,7 +121,7 @@ public class MessageActivity extends AppCompatActivity {
                 final DatabaseReference messageRecipient;
                 final DatabaseReference messageSender;
                 final String name = mAuth.getCurrentUser().getDisplayName();
-                final String message = name + ": " + messageText.getText().toString();
+                final String message = name + ": \n" + messageText.getText().toString();
                 //final String uId = mAuth.getUid();
 
                 final String uId = mAuth.getCurrentUser().getUid();
@@ -201,7 +144,7 @@ public class MessageActivity extends AppCompatActivity {
                         listItems = new ArrayList<>();
                         for (DataSnapshot msgSnapshot : dataSnapshot.getChildren()){
                             String msg0 = msgSnapshot.getValue().toString();
-                            ListItem listItem = new ListItem(name, msg0);
+                            ListItem listItem = new ListItem(" ", msg0);
                             listItems.add(listItem);
 
                             adapter = new MessageAdapter(listItems, MessageActivity.this);
@@ -215,26 +158,7 @@ public class MessageActivity extends AppCompatActivity {
 
                     }
                 });
-//                messageSender.addValueEventListener(new ValueEventListener() {
-//                    @Override
-//                    public void onDataChange(DataSnapshot dataSnapshot) {
-//                        listItems = new ArrayList<>();
-//                        for (DataSnapshot msgSnapshot : dataSnapshot.getChildren()){
-//                            String msg0 = msgSnapshot.getValue().toString();
-//                            ListItem listItem = new ListItem(name, msg0);
-//                            listItems.add(listItem);
-//
-//                            adapter = new MessageAdapter(listItems, MessageActivity.this);
-//                            recyclerView.setAdapter(adapter);
-//                            Log.d("ASDFASFGAS::",msg0);
-//                        }
-//                    }
-//
-//                    @Override
-//                    public void onCancelled(DatabaseError databaseError) {
-//
-//                    }
-//                });
+
 
             }
         });
